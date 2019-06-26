c1e021e377f1fa1dfcd529b2391110aa2311f8f5
Chris-karanja <chriswainaina0@gmail.com>
2019-06-26 22:41:58 +0300

    made the search bar console log the input

--- my-app/src/components/layout/users/Search.js
+++ my-app/src/components/layout/users/Search.js
@@ -1,11 +1,27 @@
 import React, { Component } from 'react';
 
 export class Search extends Component {
+  state = {
+    text: ''
+  };
+  onSubmit = e => {
+    e.preventDefault();
+    console.log(this.state.text);
+  };
+
+  onChange = e => this.setState({ [e.target.name]: e.target.value });
+
   render() {
     return (
       <div>
-        <form className='form'>
-          <input type='text' name='text' placeholder='search Users...' />
+        <form onSubmit={this.onSubmit} className='form'>
+          <input
+            type='text'
+            name='text'
+            placeholder='search Users...'
+            value={this.state.text}
+            onChange={this.onChange}
+          />
           <input
             type='submit'
             value='search'
