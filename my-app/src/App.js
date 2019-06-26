984bc5b80245de4c003b0a801b8bb81c715a7d68
Chris-karanja <chriswainaina0@gmail.com>
2019-06-26 08:23:21 +0300

    added a User component and made it abit more apealing

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,12 +1,16 @@
 import React, { Component } from 'react';
 import Navbar from './components/layout/Navbar';
+import User from './components/layout/users/User';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className='App'>
-        <Navbar title='Github Finder' icon='fab fa-github' />
+        <Navbar />
+        <div className='container'>
+          <User />
+        </div>
       </div>
     );
   }
--- my-app/src/components/layout/Navbar.js
+++ my-app/src/components/layout/Navbar.js
@@ -1,6 +1,11 @@
 import React, { Component } from 'react';
 
 export class Navbar extends Component {
+  static defaultProps = {
+    title: 'Github Finder',
+    icon: 'fab fa-github'
+  };
+
   render() {
     return (
       <div className='navbar bg-primary'>
